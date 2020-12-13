0f8776201035ba048816c6b6949cd6abb8aefa70
Epinder <epinder.singh@gmail.com>
2020-12-13 08:51:40 +0800

    layout.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
+import './App.module.css';
 
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
   }
 }
 
