cf2bb0be0d49f283d9042d2a5998b9a097e2d8bd
Epinder <epinder.singh@gmail.com>
2021-01-01 10:58:58 +0800

    error

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import BuildControl from './BuildControl/BuildControl';
 import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
   { label: 'Salad', type: 'salad' },
@@ -13,7 +13,7 @@ const controls = [
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
     <p>
-      Current Price: $ <strong>{props.price.toFixed(2)}</strong>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
     </p>
     {controls.map((ctrl) => (
       <BuildControl
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,11 +21,11 @@ class OrderSummary extends React.Component {
         <p>A delicious burger with the following ingredients:</p>
         <ul>{ingredientSummary}</ul>
         <p>
-          <strong>Total Price : {this.props.price.toFixed(2)}</strong>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
         </p>
         <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancled}>
-          CANCLE
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
         </Button>
         <Button btnType="Success" clicked={this.props.purchaseContinued}>
           CONTINUE
