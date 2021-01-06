d119e04bc8906af54c227c15ced7509ad83e7773
Epinder <epinder.singh@gmail.com>
2021-01-06 20:34:45 +0800

    Order summary

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -18,7 +18,8 @@ class Orders extends Component {
         for (let key in res.data) {
           fetchedOrders.push({ ...res.data[key], id: key });
         }
-        this.setState({ loading: false });
+
+        this.setState({ loading: false, orders: fetchedOrders });
       })
       .catch((err) => this.setState({ loading: false }));
   }
@@ -27,7 +28,11 @@ class Orders extends Component {
     return (
       <div>
         {this.state.orders.map((order) => (
-          <Order key={order.id} />
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
         ))}
       </div>
     );
