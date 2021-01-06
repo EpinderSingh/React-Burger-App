bc4fcf3cb92fa9394025f39a43233a9069f222d2
Epinder <epinder.singh@gmail.com>
2021-01-06 13:14:32 +0800

    routing with making order

--- src/containers/BugerBuilder/BurgerBuilder.js
+++ src/containers/BugerBuilder/BurgerBuilder.js
@@ -100,29 +100,7 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('YOU CONTINUE!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Epinder Singh',
-    //     address: {
-    //       streer: 'TestStreet 1',
-    //       postCode: '6000',
-    //       country: 'Australia',
-    //     },
-    //     email: 'epinder@epinder.com',
-    //   },
-    //   deliveryMethod: 'fastest',
-    // };
-    // axios
-    //   .post('/orders.json', order)
-    //   .then((response) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
+
     const queryParams = [];
 
     for (let i in this.state.ingredients) {
@@ -132,6 +110,7 @@ class BurgerBuilder extends Component {
           encodeURIComponent(this.state.ingredients[i]),
       );
     }
+    queryParams.push('price=' + this.state.totalPrice);
 
     const queryString = queryParams.join('&');
 
